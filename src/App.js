b525ef89efc6c69323d145411482c0c58231eaf6
itsrahulhere <rahulofspv@gmail.com>
2021-05-17 13:55:19 +0530

    new

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="left">
+    
+     </div>
+     <div className="right">
+     <div className="right-top">
+      <div className="right-top-head">
+       Motivation
+       </div>  
+       <div className="right-top-head-profile">
+       
+      </div>  
+      </div>  
+      <div className="right-bottom">
+      </div>   
+     </div>
+    </div>
+  );
+}
+
+export default App;
