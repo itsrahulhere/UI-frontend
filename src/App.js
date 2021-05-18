5c3b3bec8d8fcd188e1145691ff91d418ab7d852
itsrahulhere <rahulofspv@gmail.com>
2021-05-18 17:56:21 +0530

    18-May-21

--- src/App.js
+++ src/App.js
@@ -3,47 +3,90 @@ import './App.css';
 import {Button,Row,Col,Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import RightGrid from './RightGrid';
+import profile from './Images/profile.jpg';
+import B from './Images/B.jpg';
+import img1 from './Images/img1.jpg';
+import img2 from './Images/img2.jpeg';
+import img3 from './Images/img3.jpg';
+import img4 from './Images/img4.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus,faRocket } from '@fortawesome/free-solid-svg-icons';
+
 function App() {
   return (
     <div className="App">
    <Container>
     
      <div className="left">
+     
      <Row>
        <Col >
          <div className="left-area">
          <div className="profile">
+         <img src={B} alt="B" style={{width: 55, height: 55,borderRadius:"15px 0 15px 0",margin:"15px 0 60px 0"}}/>
+           <Row><Col xs sm md lg="2">
+           
+         <img src={profile} alt="profile" style={{width: 55, height: 55,borderRadius:"100px"}}/>
+         </Col>
+         <Col xs sm md lg="auto">
          <div className="is-active-name">
            Anand Agarwal
           </div>
           <div className="gmail">
            anand.a@bk.com
          </div>
+         </Col>
+         </Row>
          </div>
+         <br/>
         <a href="abc" class="nav-item" className="is-active-left">Motivation</a><br /><br />
+        
         <a href="abc" class="nav-item">Productivity</a>
-        <Button class="btn btn-secondary " id="leftButton">Play All</Button><br/><br/>
+        <Button class="btn btn-secondary " id="leftButton">Home Design</Button><br/><br/>
         <a href="abc" class="nav-item">Design</a><br/><br/>
-        <a href="abc" class="nav-item">Study</a><br/><br/>
+        <a href="abc" class="nav-item">Study</a><br/><br/><br/>
         <div className="left-bottom">
         <Container>
         <Row>
-          <Col></Col>
+          <Col xs sm md lg="2"> 
+      <FontAwesomeIcon className="Rocket" icon={faRocket} size="2x"/>
+      </Col>
           <Col sm="10">Pro account is more powerful. Get 30% off
           <Button class="btn btn-secondary " id="SeePro">See Pro</Button><br/><br/>
           </Col>
           </Row>
           </Container>
-        </div><br/><br/>
+        </div><br/>
+        <Row><Col>
         <a href="abc" class="nav-item" className="is-active-left">Logout</a><br /><br />
-        </div>
+        </Col><Col xs sm md lg="2">
+        <div className="leftPlus">
+      <FontAwesomeIcon icon={faPlus}/>
+      </div>
+      </Col>
+      </Row>
+      </div>
         </Col>
       <Col xs sm md lg = "8">
      <div className="right">
      <div className="right-top">
       <div className="right-top-head">
+        <Row>
+          <Col xs sm md lg="9">
       <h1 className="Motivation"><b>Motivation</b></h1>
-       
+      </Col>
+      <Col xs sm md lg="3">
+      <div className="rightprofiles">
+      <img className="img1" src={img1} alt="profile" style={{width: 40, height: 40,borderRadius:"100px"}}/>
+      <img className="img2" src={img2} alt="profile" style={{width: 40, height: 40,borderRadius:"100px"}}/>
+      <img className="img3" src={img3} alt="profile" style={{width: 40, height: 40,borderRadius:"100px"}}/>
+      <img className="img4"src={img4} alt="profile" style={{width: 40, height: 40,borderRadius:"100px"}}/>
+      <div className="Plus">
+      <FontAwesomeIcon icon={faPlus}/>
+      </div>
+      </div>
+      </Col>
+      </Row>
        <nav class="nav">
           <a href="abc" class="nav-item " active-color="white"><div className="is-active">All</div></a>
           <a href="abc" class="nav-item">Articles</a>
--- src/RightGrid.js
+++ src/RightGrid.js
@@ -1,20 +1,59 @@
 import './App.css';
 import './RightGrid.css';
-import {Row,Col,Container} from 'react-bootstrap';
+import {Button,Row,Col,Container} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay } from '@fortawesome/free-solid-svg-icons';
 function RightGrid() {
   return (
     <div className="RightGrid">
             <Container>
                 <Row>
-                    <Col>
+                <Col sm xs md lg="5">
+                  <Col>
                     <div className="firstBox">
-                    Your<br/>limitation<br/>it's only<br/>your<br/> imagination.
+                   <b>Your<br/>limitation<br/>it's only<br/>your<br/> imagination.</b> 
+                    <div className="Play">
+                            <FontAwesomeIcon  icon={faPlay} size="xs"/>
+                    </div>
                     </div>
                     </Col>
                     <Col>
+                    <div className="fourthBox">
+                    
+                    </div>
                     </Col>
-                    <Col>
+                    </Col>
+                    <Col><Row>
+                    <Col sm xs md lg="6">
+                    <div className="secondBox">
+                   <b>Great things<br/>never come from<br/>comfort zones.</b> 
+                   <div className="Play2">
+                            <FontAwesomeIcon  icon={faPlay} size="xs"/>
+                    </div>
+                    </div>
+                    </Col>
+                    <Col sm xs md lg="6">
+                    <div className="thirdBox">
+                   <b>Dream it.<br/>Wish it.<br/>Do it.</b> 
+                    </div>
+                    </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                    <div className="fifthBox">
+
+                   <b>Stay <br/>Focused.</b><br/>
+                   <Button class="btn btn-secondary " id="StayFocusedBtn">Good Articles</Button><br/><br/>
+                    </div>
+                    </Col>
+                    </Row>
+                    <Row>
+                      <Col>
+                    <div className="sixthBox">
+                    </div>
+                    </Col>
+                    </Row>
                     </Col>
                 </Row>
             </Container>
